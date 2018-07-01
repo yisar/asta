454dee639bae8a01358ab7a01edba0a3870e373d
132yse <1533540012@qq.com>
2018-07-01 23:14:42 +0800

    修复bug，更新逻辑

--- src/store.js
+++ src/store.js
@@ -2,50 +2,52 @@ import { resolveSource, splitType } from './util'
 import { combineModels } from './helper'
 import { produce } from './produce'
 export class Store {
-    constructor(models) {
-        this.state = models.state ? models.state : combineModels(models).state
-        this.mutations = models.state ? models.mutations : combineModels(models).mutations
-        this.actions = models.state ? models.actions : combineModels(models).actions
-        this.subscribers = []
-        this.dispatch = this.dispatch.bind(this)
-        this.commit = this.commit.bind(this)
+  constructor(models) {
+    this.state = models.state ? models.state : combineModels(models).state
+    this.mutations = models.state ? models.mutations : combineModels(models).mutations
+    this.actions = models.state ? models.actions : combineModels(models).actions
+    this.subscribers = []
+    this.dispatch = this.dispatch.bind(this)
+    this.commit = this.commit.bind(this)
+  }
+  subscribe(sub) {
+    return this.subscribers.push(sub)
+  }
+  unsubscribe(sub) {
+    return this.subscribers.filter(f => f !== sub)
+  }
+  dispatch(type, payload, name) {
+    if (name) {
+      payload = {
+        name,
+        payload
+      }
     }
-    subscribe(sub) {
-        return this.subscribers.push(sub)
+    const action = resolveSource(this.actions, type)
+    const ctx = {
+      commit: this.commit,
+      dispatch: this.dispatch
     }
-    unsubscribe(sub) {
-        return this.subscribers.filter(f => f !== sub)
+    return Promise.resolve(action(ctx, payload))
+  }
+  commit(type, payload, name) {
+    if (payload.name) {
+      name = payload.name
+      payload = payload.payload
     }
-    dispatch(type, payload, name) {
-        if (name) {
-            payload = {
-                name,
-                payload
-            }
-        }
-        console.log(type)
-        const action = resolveSource(this.actions, type)
-        const ctx = {
-            commit: this.commit,
-            dispatch: this.dispatch
-        }
-        return Promise.resolve(action(ctx, payload))
-    }
-    commit(type, payload, name) {
-        if (payload.name) {
-            name = payload.name
-            payload = payload.payload
-        }
-        type = splitType(type)
-        const mutation = resolveSource(this.mutations, type, name)
-        const model = Array.isArray(type) ? type[0] : name
-        typeof type === 'function' ?
-            this.state[name] = produce(this.state[name], state => {
-                mutation(state, payload)
-            }) :
-            this.state[model] = produce(this.state[model], state => {
-                mutation(state, payload)
-            })
-        this.subscribers.forEach(v => v())
-    }
+    type = splitType(type)
+    const mutation = resolveSource(this.mutations, type, name)
+    const model = Array.isArray(type) ? type[0] : name
+    typeof type === 'function' && name ?
+      this.state[name] = produce(this.state[name], state => {
+        mutation(state, payload)
+      }) : model ?
+        this.state[model] = produce(this.state[model], state => {
+          mutation(state, payload)
+        }) :
+        this.state = produce(this.state, state => {
+          mutation(state, payload)
+        })
+    this.subscribers.forEach(v => v())
+  }
 }
--- src/util.js
+++ src/util.js
@@ -1,34 +1,36 @@
 export function resolveSource(source, type, name) {
-    if (typeof type === 'function') {
-        return type
-    }
-    return Array.isArray(type) ? source[type[0]][type[1]] : name ? source[name][type] : source[type]
+  if (typeof type === 'function') {
+    return type
+  }
+  return Array.isArray(type) ? source[type[0]][type[1]] : name ? source[name][type] : source[type]
 }
 export function bindCreators(creators, operate, name) {
-    return Object.keys(creators).reduce((ret, item) => {
-        ret[item] = (...args) => operate(creators[item], ...args, name)
-        return ret
-    }, {})
+  return Object.keys(creators).reduce((ret, item) => {
+    ret[item] = (...args) => operate(creators[item], ...args, name)
+    return ret
+  }, {})
 }
 export function normalizeMap(map) {
-    return Array.isArray(map)
-        ? map.map(k => ({ k, v: k }))
-        : Object.keys(map).map(k => ({ k, v: map[k] }))
+  return Array.isArray(map)
+    ? map.map(k => ({ k, v: k }))
+    : Object.keys(map).map(k => ({ k, v: map[k] }))
 }
 
 export function splitContent(type) {
-    let res = []
-    type.map(i => {
-        res.push(i.split('/')[1])
-    })
-    return res
+  let res = []
+  type.map(i => {
+    type.indexOf('/') != -1 ?
+      res.push(i.split('/')[1]) :
+      res.push(i)
+  })
+  return res
 }
 
 export function splitType(type) {
-    if (typeof type === 'function') {
-        return type
-    }
-    return type.indexOf('/') != -1 ?
-        type.split('/') : type
+  if (typeof type === 'function') {
+    return type
+  }
+  return type.indexOf('/') != -1 ?
+    type.split('/') : type
 
 }
