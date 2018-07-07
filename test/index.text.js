0daf4f78619754f868d8ef5c916d6e0fbc09790a
132yse <1533540012@qq.com>
2018-07-07 22:50:18 +0800

    commit dayly

--- test/index.text.js
+++ test/index.text.js
@@ -1,12 +1,12 @@
-import { resolveSource, normalizeMap, splitType } from '../src/utils'
-
-test('test resolveSource', () => {
-  expect(resolveSource(action, add)).toBe(action[add])
-})
+import { resolveSource, normalizeMap, splitType, splitContent } from '../src/utils'
 
 function add() {
   console.log('add')
 }
+
+test('test resolveSource', () => {
+  expect(resolveSource(action, add)).toBe(action[add])
+})
 
 test('test normalizeMap', () => {
   expect(normalizeMap(['add', 'cut'])).toBe([{ k: 'add', v: 'add' }, { k: 'cut', v: 'cut' }])
@@ -17,4 +17,8 @@ test('test splitType', () => {
   expect(splitType('add')).toBe('add')
   expect(splitType(add)).toBe(add)
   expect(splitType('count/add')).toBe(['count', 'add'])
+})
+
+test('test splitContent', () => {
+  expect(splitContent(['count/add','count/cut'])).toBe(['add','cut'])
 })
