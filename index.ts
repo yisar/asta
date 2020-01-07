57df6c646d34671591497ec70524b72556a4fac4
132yse <1533540012@qq.com>
2020-01-07 20:34:07 +0800

    add reactivity

--- index.ts
+++ index.ts
@@ -1,1 +1,46 @@
-export const setup = fn => {}
+export function setup(fn) {
+  fn.composition = true
+  return fn
+}
+
+const toProxy = new WeakMap()
+const toRaw = new WeakMap()
+
+const isObj = (x: any): x is object => typeof x === 'object'
+
+export function reactive(target) {
+  if (!isObj(target)) return target
+
+  let proxy = toProxy.get(target)
+  if (proxy) return proxy
+
+  if (toRaw.has(target)) return target
+
+  const handlers = {
+    get(target, key, receiver) {
+      let newValue = target[key]
+
+      if (isObj(newValue)) {
+        return reactive(newValue)
+      }
+      let res = Reflect.get(target, key, receiver)
+    //   track(target, key)
+      return res
+    },
+    set(target, key, value, receiver) {
+      let res = Reflect.set(target, key, value, receiver)
+    //   if (key in target) trigger(target, key)
+      return res
+    },
+    deleteProperty(target, key, receiver) {
+      return Reflect.defineProperty(target, key, receiver)
+    }
+  }
+
+  let observed = new Proxy(target, handlers as any)
+
+  toProxy.set(target, observed)
+  toRaw.set(observed, target)
+
+  return observed
+}
