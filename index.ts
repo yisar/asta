57b0cc7410b1a7f74fbf492ea5a16c287d5b0eb0
132yse <1533540012@qq.com>
2020-01-08 09:35:35 +0800

    add effect

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
 import { useState, useCallback } from 'react'
 
-export function setup(fn) {
-  fn.composition = true
-  return fn
-}
+const activeEffect = []
+export const targetMap = new WeakMap()
 
 const toProxy = new WeakMap()
 const toRaw = new WeakMap()
@@ -26,11 +24,12 @@ export function reactive(target) {
         return reactive(newValue)
       }
       let res = Reflect.get(target, key, receiver)
+      track(target, key)
       return res
     },
     set(target, key, value, receiver) {
       let res = Reflect.set(target, key, value, receiver)
-      if (key in target) forceUpdate()
+      if (key in target) trigger(target, key)
       return res
     },
     deleteProperty(target, key, receiver) {
@@ -43,10 +42,60 @@ export function reactive(target) {
   toProxy.set(target, observed)
   toRaw.set(observed, target)
 
+  if (!targetMap.has(target)) {
+    targetMap.set(target, new Map())
+  }
+
   return observed
 }
 
-function forceUpdate() {
-  const update = useState()[0]
-  return useCallback(() => update({}), [])
+export function setup(fn){
+  const update = applyEffect(()=>{
+    const [,setState] = useState({})
+    setState({})
+  })
+  update()
+}
+
+function applyEffect(fn) {
+  const effect = function effect(...args) {
+    return run(effect, fn, args)
+  }
+  return effect
+}
+
+function run(effect, fn, args) {
+  if (activeEffect.indexOf(effect) === -1) {
+    try {
+      activeEffect.push(effect)
+      return fn(...args)
+    } finally {
+      activeEffect.pop()
+    }
+  }
+}
+
+export function trigger(target, key) {
+  let deps = targetMap.get(target)
+  const effects = new Set()
+
+  deps.get(key).forEach(e => effects.add(e))
+  effects.forEach((e: any) => e())
+}
+
+export function track(target, key) {
+  const effect = activeEffect[activeEffect.length - 1]
+  if (effect) {
+    let depsMap = targetMap.get(target)
+    if (!depsMap) {
+      targetMap.set(target, (depsMap = new Map()))
+    }
+    let dep = depsMap.get(key)
+    if (!dep) {
+      depsMap.set(key, (dep = new Set()))
+    }
+    if (!dep.has(effect)) {
+      dep.add(effect)
+    }
+  }
 }
