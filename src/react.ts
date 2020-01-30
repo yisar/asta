5625bd76fb8aedcc6a4da6b3582df3d5384d231b
伊撒尔 <1533540012@qq.com>
2020-01-30 15:09:40 +0800

    Update react.ts

--- src/react.ts
+++ src/react.ts
@@ -1,18 +1,16 @@
 import * as React from 'react'
 import { watch, unwatch, reactive, raw, isReactive } from './reactivity'
 
-function setup(factory) {
-  let FN = null
-  const wrapped = React.memo(props => {
+function setup(factory, ) {
+  return React.memo(props => {
     if (!FN) FN = factory(props)
     const update = React.useReducer(s => s + 1, 0)[1]
     let vdom = null
-    const reaction = watch(() => (vdom = FN(props)), {
+    watch(() => (vdom = FN(props)), {
       scheduler: () => update()
     })
     return vdom
   })
-  return wrapped
 }
 
 export { setup, watch, unwatch, reactive, raw, isReactive }
