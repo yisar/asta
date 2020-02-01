47ceb164a3ed75752e54b6916a0262fe02000abc
132yse <1533540012@qq.com>
2020-02-01 12:55:33 +0800

    emmm

--- src/react.ts
+++ src/react.ts
@@ -3,20 +3,18 @@ import { watch, unwatch, reactive, computed, ref, raw, isReactive, isRef } from 
 
 function setup<T>(factory: T, render: unkown) {
   return React.memo(props => {
-    return React.memo(props => {
-      const w = React.useRef()
-      const r = React.useRef()
-      const update = React.useReducer(s => s + 1, 0)[1]
-      if (!r.current) r.current = factory(props)
-      let getter = typeof r.current === 'function' ? () => r.current(props) : () => factory(props)
-      if (!w.current) {
-        w.current = watch(getter, {
-          scheduler: () => Promise.resolve().then(update)
-        })
-      }
-      React.useEffect(() => () => unwatch(w.current), [])
-      return w.current()
-    })
+    const w = React.useRef()
+    const r = React.useRef()
+    const update = React.useReducer(s => s + 1, 0)[1]
+    if (!r.current) r.current = factory(props)
+    let getter = typeof r.current === 'function' ? () => r.current(props) : () => factory(props)
+    if (!w.current) {
+      w.current = watch(getter, {
+        scheduler: () => Promise.resolve().then(update)
+      })
+    }
+    React.useEffect(() => () => unwatch(w.current), [])
+    return w.current()
   })
 }
 
