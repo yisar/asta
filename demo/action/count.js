ce7e09bba65eff28e072057797457bf939795cf9
zhaochanghao <zhaochanghao@xd.com>
2022-10-21 17:20:18 +0800

    async action

--- demo/action/count.js
+++ demo/action/count.js
@@ -1,4 +1,5 @@
-export const addCount = (state, event) => {
+export const addCount = async (state, event) => {
+    await new Promise(r => setTimeout(() => r(), 1000))
     return {
         ...state,
         count: state.count + 1,
